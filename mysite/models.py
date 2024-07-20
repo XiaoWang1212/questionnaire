965bd25464cada97ab1afab36964efe8fdc2d0ee
XiaoWang1212 <danny20041212@gmail.com>
2024-07-21 01:25:16 +0800

    init

--- mysite/models.py
+++ mysite/models.py
@@ -2,24 +2,6 @@ from typing import Any
 from django.db import models
 
 # Create your models here.
-
-class Mood(models.Model):
-    status = models.CharField(max_length=10, null=False)
-    
-    def __str__(self):
-        return self.status
-    
-class Post(models.Model):
-    mood = models.ForeignKey('Mood', on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=10, default="不願透漏身分的人")
-    message = models.TextField(null=False)
-    del_pass = models.CharField(max_length=10)
-    pub_time = models.DateTimeField(auto_now=True)
-    enabled = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.message
-    
 class Questionnaire(models.Model):
     name = models.CharField(max_length=4)
     student_ID = models.CharField(max_length=10)
