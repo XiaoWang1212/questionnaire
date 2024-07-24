29007ba04caf1ab0ec470b3c3225a46bcb611cc3
XiaoWang1212 <danny20041212@gmail.com>
2024-07-25 03:18:44 +0800

    db changed

--- xiaowang/settings.py
+++ xiaowang/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = "xiaowang.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
 '''
@@ -93,7 +93,7 @@ DATABASES = {
         'PORT': '3306',
     },
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
