fba8b5fe7e96fe6153dc6999f60e6a3a934e301e
weihong931021 <88928909+weihong931021@users.noreply.github.com>
2024-07-29 04:15:14 +0800

    test3

--- xiaowang/settings.py
+++ xiaowang/settings.py
@@ -151,5 +151,7 @@ EMAIL_HOST_PASSWORD = "vavolqogwnqjyfvh"
 
 GOOGLE_RECAPTCHA_SECRET_KEY = "6LdrQBIqAAAAANUSZTZ9ZYMZgLXlIMyz6kzf7A4o"
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
